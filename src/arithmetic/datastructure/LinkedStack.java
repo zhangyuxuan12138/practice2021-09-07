4001b9a5fe3393bfde2b2c9def844bc366b37174
zhangyuxuan28 <zhangyuxuan28@jd.com>
2021-10-17 21:21:52 +0800

    链表实现堆结构

--- src/arithmetic/datastructure/LinkedStack.java
+++ src/arithmetic/datastructure/LinkedStack.java
@@ -9,22 +9,28 @@ public class LinkedStack<T> {
     private static class Node<T> {
         T item;
         Node<T> next;
+
         Node() {
             item = null;
             next = null;
         }
+
         Node(T item, Node<T> next) {
             this.item = item;
             this.next = next;
         }
+
         boolean end() {
             return item == null && next == null;
         }
     }
+
     private Node<T> top = new Node<>();
+
     public void push(T item) {
         top = new Node<>(item, top);
     }
+
     public T pop() {
         T result = top.item;
         if (!top.end()) {
